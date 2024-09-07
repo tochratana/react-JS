458ae41c192c8e1abdde869f92be30abaedac637
Toch Ratana <ltomusic69@gmail.com>
2024-09-07 23:18:54 +0700

    AddTailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDom from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDom.createRoot(document.getElementById("root")).render(<App />);
+
+// console.log(root);
