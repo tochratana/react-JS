4fa397a0ff6f94198c8070034ebee4a30b032f8f
RML-Cambo <ltomusic69@gmail.com>
2024-09-24 21:51:48 +0700

    Edu

---
+++ src/components/main/BtnLanguage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const BtnLanguage = () => {
+  return (
+    <>
+      <button
+        id="dropdownHoverButton"
+        data-dropdown-toggle="dropdownHover"
+        data-dropdown-trigger="hover"
+        class="text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center w-28 dark:bg-gray-800 mr-2"
+        type="button"
+      >
+        Language
+      </button>
+
+      <div
+        id="dropdownHover"
+        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-28 dark:bg-gray-800"
+      >
+        <ul
+          class="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownHoverButton"
+        >
+          <li>
+            <a
+              href="#"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              English
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              ខ្មែរ
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default BtnLanguage;
