f0c151d1e4d5654a8c6f3aea988927bbd7aaa6d9
RML-Cambo <ltomusic69@gmail.com>
2024-09-30 18:43:53 +0700

    profile

--- src/page/about/Project.jsx
+++ src/page/about/Project.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import demoAnimate from "../../../public/Svg/demo.svg";
+import businessweb from "../../../public/image/p_business.png";
+import por from "../../../public/image/p_por.png";
+import service from "../../../public/image/p_service.png";
+import teamwork from "../../../public/image/p_teamwork.png";
 
 const Project = () => {
   return (
@@ -36,7 +40,57 @@ const Project = () => {
         <section className="flex flex-col items-center">
           <h1 className="text-3xl font-bold mb-5">Some of My project</h1>
           <p id="demo">You can demo it</p>
-          <ProjectCard />
+          {/* <ProjectCard /> */}
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-20">
+            <div className="port-box">
+              <div className="prot-image">
+                <img src={businessweb} />
+              </div>
+              <div className="port-content">
+                <h3>BusinessWeb</h3>
+                <p>Help you to find your client and easy business</p>
+                <a href="#">
+                  <i className="fa-solid fa-up-right-from-square text-black"></i>
+                </a>
+              </div>
+            </div>
+            <div className="port-box">
+              <div className="prot-image">
+                <img src={por} />
+              </div>
+              <div className="port-content">
+                <h3>TeamWork</h3>
+                <p>Find work team and tranning with your career</p>
+                <a href="#">
+                  <i className="fa-solid fa-up-right-from-square text-black"></i>
+                </a>
+              </div>
+            </div>
+            <div className="port-box">
+              <div className="prot-image">
+                <img src={service} />
+              </div>
+              <div className="port-content">
+                <h3>Portfolio Demo</h3>
+                <p>Simple Portfolio for first leaning</p>
+                <a href="#">
+                  <i className="fa-solid fa-up-right-from-square text-black"></i>
+                </a>
+              </div>
+            </div>
+            <div className="port-box">
+              <div className="prot-image">
+                <img src={teamwork} />
+              </div>
+              <div className="port-content">
+                <h3>Service</h3>
+                <p>Provide you about service with your daily</p>
+                <a href="#">
+                  <i className="fa-solid fa-up-right-from-square text-black"></i>
+                </a>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </>
